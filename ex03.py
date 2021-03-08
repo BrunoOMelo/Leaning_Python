699e7ad33e0ded740d8d4e58454a6cb5197216fd
BrunoOMelo <78369748+BrunoOMelo@users.noreply.github.com>
2021-03-08 11:58:33 -0300

    Exercise 03
    Variable formatting for show the correct sum result .

---
+++ ex03.py
@@ -0,0 +1,5 @@
+#declaring and formatting multples variables as integer
+num01= int(input('Type the first number: '))
+num02= int(input('Type the second number: '))
+#showing to user the sum of numbers
+print('The sum of the first and the second number is:', num01 + num02)
