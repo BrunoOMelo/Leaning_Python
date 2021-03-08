0a0fb9cc67c3a7a4cf7354622cf1d74f82af1c30
BrunoOMelo <78369748+BrunoOMelo@users.noreply.github.com>
2021-03-08 11:56:02 -0300

    Exercise 02
    Learning multiple variables declaration and using print whith text and variables.

---
+++ ex02.py
@@ -0,0 +1,6 @@
+#declaring multiples variables and using the command "input".
+day= input('What is your birth day?:  ')
+month= input('What is your birth month?: ')
+year= input('What is your birth year?: ')
+#Showing to user a message with his dates throught the command "print"
+print('You were born in the day', day,', in the month', month,'and in the year',year,'.')
