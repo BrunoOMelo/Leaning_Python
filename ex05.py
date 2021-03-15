c307e5dd4d7c6fe0cbd2ef5712a4d2bee95e16e0
BrunoOMelo <78369748+BrunoOMelo@users.noreply.github.com>
2021-03-15 10:14:26 -0300

    Exercise 05
    Using '.format' for show multiples variables.

---
+++ ex05.py
@@ -0,0 +1,6 @@
+#declaring and formatting multiples variables as integer.
+num01 = int(input('Type the first number: '))
+num02 = int(input('Type the second number: '))
+s =  num01 + num02
+#showing to user the sum of numbers.
+print('The sum of {} and {} is: {}' .format(num01, num02, s))
