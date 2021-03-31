24aaa90d91650e234ac7ca7b1e73693a2012eac8
Bruno de Oliveira Melo <78369748+BrunoOMelo@users.noreply.github.com>
2021-03-31 15:09:06 -0300

    Exercise 10.
    Converting R$ in US$ using the "input" of price

---
+++ ex10.py
@@ -0,0 +1,5 @@
+#declaring and formatting variable as float.
+real = float(input('Enter the amounth of R$: '))
+dolar = float(input('Enter the US$ price: '))
+#presenting the conversion
+print('R$ {:.2f} in US$: {:.2f}'.format(real,real/dolar))
