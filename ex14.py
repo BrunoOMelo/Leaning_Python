3ef309c0f4df633d10feb0b40963dd1905638725
Bruno de Oliveira Melo <78369748+BrunoOMelo@users.noreply.github.com>
2021-04-12 14:20:22 -0300

    Exercise 14.
    update the comments.

--- ex14.py
+++ ex14.py
@@ -1,8 +1,12 @@
+#importing only one funciotion from library 
 from random import shuffle
+#declaring variables as string
 n1 = str(input('Fist name: '))
 n2 = str(input('Second name: '))
 n3 = str(input('Third name: '))
 n4 = str(input('Fourth name: '))
+#creating a list with this variables
 lista = [n1,n2,n3,n4]
+#using 'shufle' for change the order of list
 shuffle(lista)
 print(lista)
